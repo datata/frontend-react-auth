cbbea6fc8e0cdcda2049d0ffff65d6c6b358d4a5
datata <dani@danitarazona.dev>
2023-02-05 21:15:19 +0100

    fix: clean code rows

--- src/views/TaskList/TaskList.jsx
+++ src/views/TaskList/TaskList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { TaskCard } from '../../components/task/TaskCard';
 import { updateTasks } from '../../feature/tasks/taskSlice';
